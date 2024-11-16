a4fbb9014e662902bff0f3de06f2dc9ea0f40457
albankerloch <54630416+albankerloch@users.noreply.github.com>
2024-11-16 12:36:25 +0100

    Deploiement function-1

--- Cloud Functions/function-1-download/main.py
+++ Cloud Functions/function-1-download/main.py
@@ -1,20 +1,23 @@
 import os
 import json
+import tempfile
+import git
 import functions_framework
-import vertexai
 
 from google.cloud import logging
-from vertexai.preview.generative_models import GenerativeModel, Part
+from google.cloud import storage
 
 PROJECT_ID = "doxygen-gcp"
 LOCATION = "europe-west1"
 BUCKET = "doxygen-gcp-storage"
+LOG_NAME = "run_inference-cloudfunction-download-log"
 
 client = logging.Client(project=PROJECT_ID)
 client.setup_logging()
-
-LOG_NAME = "run_inference-cloudfunction-download-log"
 logger = client.logger(LOG_NAME)
+
+storage_client = storage.Client()
+bucket = storage_client.bucket(BUCKET)
 
 @functions_framework.http
 def run_inference(request):
@@ -37,7 +40,22 @@ def run_inference(request):
 
     logger.log(f"URL request for prompt: {url}")
 
-    storage_uri = "to do"
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # Cloner le répertoire Git
+        repo = git.Repo.clone_from(url, tmpdirname)
+        repo_name = os.path.basename(url).replace('.git', '')
+
+        # Référence au bucket
+        # Parcourir les fichiers du répertoire local et les télécharger dans GCS
+        for root, dirs, files in os.walk(tmpdirname):
+            for file in files:
+                local_file_path = os.path.join(root, file)
+                blob_path = os.path.join(repo_name, os.path.relpath(local_file_path, tmpdirname))
+                blob = bucket.blob(blob_path)
+                blob.upload_from_filename(local_file_path)
+                logger.log(f'Téléchargé {local_file_path} vers gs://{BUCKET}/{blob_path}')
+
+    storage_uri = repo_name
 
     logger.log(f"Git repository downloaded a: {storage_uri}")
 
